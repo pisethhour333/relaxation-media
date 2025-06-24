1db0de11a1242cb87cbf6a40de63bfcb20bbfc53
pisethhour333 <pisethus@gmail.com>
2025-06-24 11:34:08 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggleButton = document.getElementById("mode-toggle");
-    const body = document.body;
-
-    toggleButton.addEventListener("click", () => {
-        body.classList.toggle("dark-mode");
-    });
-});
